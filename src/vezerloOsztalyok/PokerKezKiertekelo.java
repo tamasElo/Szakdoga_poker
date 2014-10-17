cd08a1bbfe63adc13ab7035b32673c2b6db06f93
Tomi <Tomi@Tomi-PC>
2014-10-17 16:56:46 +0200

    A póker kéz kiértékelő osztály megírva

--- src/vezerloOsztalyok/PokerKezKiertekelo.java
+++ src/vezerloOsztalyok/PokerKezKiertekelo.java
@@ -9,14 +9,6 @@ import java.util.List;
 import java.util.Map;
 
 public class PokerKezKiertekelo {
-    /*
-     A Jatekos osztályban kell csinálni egy listát ami letárolja az aktuális 
-     lapkombinációt illetve az öt legmagasabb lapot és egy String-et ami a kombináció neve pl:flöss.
-     Így ha döntetlen helyzet van akkor pl collections.max() függvénnyel meg 
-     lehet keresni hogy kinek van pl párja, drillje stb. ha egyeznek,
-     akkor a kísérő lapoktól függ hogy ki nyer. Ha az is egyforma, akkor 
-     döntetlen és osztoznak a nyertesek. Ha van maradék akkor az osztótól balra ülő játékos kapja.
-     */
     private static List<Byte> nyertesJatekosokSorszama;
     private static Map<Byte, PokerKez> jatekosokPokerKeze;
     private static byte legmagasabbPokerKezErtek;
